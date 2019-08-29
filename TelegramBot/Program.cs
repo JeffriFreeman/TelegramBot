eb0f369a50bbc29952f66e960d94f901a714f9e0
Гогуев Никита Вадимович <n.goguev@socium-a.net>
2019-08-29 18:45:57 +0300

    Add project files.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        private static readonly Dictionary<string, string> data;
+
+        static Program()
+        {
+            var d = File.ReadAllText(@"C:\Users\n.goguev\source\repos\TelegramBot\TelegramBot\answerData.json");
+            data = JsonConvert.DeserializeObject<Dictionary<string, string>>(d);
+            
+        }
+        static void Main(string[] args)
+        {
+            var api = new TelegramAPI();
+            api.SendApiRequest("getMe");
+            //while (true)
+            //{
+            //    if (AnswerQuetion() == false)
+            //    {
+            //        break;
+            //    }
+            //}
+            Console.ReadLine();
+        }
+
+        private static bool AnswerQuetion()
+        {
+            var question = Console.ReadLine().ToLower();
+            var answer = "";
+            foreach (var e in data)
+            {
+                if (question.Contains(e.Key))
+                {
+                    answer += e.Value;
+                }
+            }
+            Console.WriteLine(answer);
+            return false;
+        }
+    }
+}
---
+++ TelegramBot/TelegramAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using RestSharp;
+
+namespace TelegramBot
+{
+    class TelegramAPI
+    {
+        private const String API_KEY = "769427152:AAFURhx7Ar9bqmrpVENcEgQ27-vhmOV4ebQ";
+        private const String API_URL = "https://api.telegram.org/bot" + API_KEY + "/";
+
+        private RestClient rc = new RestClient();
+
+        public TelegramAPI()
+        {
+            rc.Proxy = new WebProxy("162.243.108.141", 8080);
+        }
+
+        public void SendApiRequest(String apiMethod)
+        {
+            //var url = API_URL + apiMethod;
+            //var request = new RestRequest(url);
+            var request = new RestRequest("https://yandex.ru/");
+            //var request = new RestRequest("");
+            Console.WriteLine("requesting");
+            var responce = rc.Get(request);
+            Console.WriteLine("requested");
+            Console.WriteLine(responce.Content);
+        }
+    }
+}
